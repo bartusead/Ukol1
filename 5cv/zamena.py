33985ee83297fc42759bd256d5dcf1907f1b685d
bartusead <93137808+bartusead@users.noreply.github.com>
2021-11-06 12:45:03 +0100

    Delete 5cv directory

--- 5cv/zamena.py
+++
@@ -1,9 +0,0 @@
-slovo = input("Zadej slovo: ")
-pozice = int(input("Kolikáté písmeno chceš zaměnit?: "))
-novy_znak = input("Zadej nové písmeno: ")
-
-zacatek_slova = slovo[:pozice]
-konec_slova = slovo[pozice + 1:]
-nove_slovo = zacatek_slova + novy_znak + konec_slova
-
-print(nove_slovo)
